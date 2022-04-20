72ed07207b4485714fdf3a3fdeb9bb3b81a2c73e
Salih Erdal <erdsal.4@gmail.com>
2022-04-20 15:29:40 -0700

    fix last index of denormalized

--- services/pg_denormalized/schema-indexes.sql
+++ services/pg_denormalized/schema-indexes.sql
@@ -1,7 +1,7 @@
--- question 1
+-- question 1,2,3
 create index on tweets_jsonb using gin ( (data->'entities'->'hashtags') );
 create index on tweets_jsonb using gin ( (data->'extended_tweet'->'entities'->'hashtags') );
 
--- question 4
+-- question 4,5
 -- create index on tweets_jsonb using gin ( (data->>'lang') );
-create index on tweets_jsonb using gin ( to_tsvector('english', coalesce(data->'extended_tweet'->>'full_text'), data->>'text') );
+create index on tweets_jsonb using gin ( (to_tsvector('english', coalesce(data->'extended_tweet'->>'full_text', data->>'text'))) );
